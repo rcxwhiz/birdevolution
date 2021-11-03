6c79904086bfddea36bcba50c81f130cf4168cb7
Josh <rcxwhiz@gmail.com>
2021-11-02 20:14:14 -0600

    Added stuff to complete the neural-network lib

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -19,14 +19,50 @@ pub struct LayerTopology {
 }
 
 impl Network {
+    pub fn new(layers: Vec<Layer>) -> Self {
+        Self { layers }
+    }
+
+    pub fn random(rng: &mut dyn RngCore, layers: &[LayerTopology]) -> Self {
+        assert!(layers.len() > 1);
+
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::random(rng, layers[0].neurons, layers[1].neurons))
+            .collect();
+
+        Self::new(layers)
+    }
+
+    pub fn from_weights(layers: &[LayerTopology], weights: impl IntoIterator<Item = f32>) -> Self {
+        assert!(layers.len() > 1);
+
+        let mut weights = weights.into_iter();
+
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::from_weights(layers[0].neurons, layers[1].neurons, &mut weights))
+            .collect();
+
+        if weights.next().is_some() {
+            panic!("got too many weights");
+        }
+
+        Self::new(layers)
+    }
+
     fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.layers
             .iter()
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
     }
 
-    pub fn new(layers: Vec<Layer>) -> Self {
-        Self { layers }
+    pub fn weights(&self) -> impl Iterator<Item = f32> + '_ {
+        self.layers
+            .iter()
+            .flat_map(|layer| layer.neurons.iter())
+            .flat_map(|neuron| once(&neuron.bias).chain(&neuron.weights))
+            .cloned()
     }
 }
 
@@ -39,6 +75,14 @@ impl Layer {
             .all(|neuron| neuron.weights.len() == neurons[0].weights.len()));
 
         Self { neurons }
+    }
+
+    pub fn from_weights(input_size: usize, output_size: usize, weights: &mut dyn Iterator<Item = f32>) -> Self {
+        let neurons = (0..output_size)
+            .map(|_| Neuron::from_weights(input_size, weights))
+            .collect();
+
+        Self::new(neurons)
     }
 
     fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
@@ -64,16 +108,6 @@ impl Neuron {
         Self { bias, weights }
     }
 
-    fn propagate(&self, inputs: &[f32]) -> f32 {
-        let output = inputs
-            .iter()
-            .zip(&self.weights)
-            .map(|(input, weight)| input * weight)
-            .sum::<f32>();
-
-        (self.bias + output).max(0.0)
-    }
-
     pub fn random(rng: &mut dyn RngCore, output_neurons: usize) -> Self {
         let bias = rng.gen_range(-1.0..=1.0);
 
@@ -82,5 +116,25 @@ impl Neuron {
             .collect();
 
         Self::new(bias, weights)
+    }
+
+    pub fn from_weights(output_neurons: usize, weights: &mut dyn Iterator<Item = f32>) -> Self {
+        let bias = weights.next().expect("got not enough weights");
+
+        let weights = (0..output_neurons)
+            .map(|_| weights.next().expect("got not enough weights"))
+            .collect();
+
+        Self::new(bias, weights)
+    }
+
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+
+        (self.bias + output).max(0.0)
     }
 }
