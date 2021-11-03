be7723bdf8c13d21bb08ad6e4faa1d6391faa28d
Josh <rcxwhiz@gmail.com>
2021-11-02 19:59:00 -0600

    I think I got the neural-network lib right?

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,7 +1,64 @@
-pub struct Network;
+use rand::Rng;
+
+pub struct Network {
+    layers: Vec<Layer>,
+}
+
+struct Layer {
+    neurons: Vec<Neuron>,
+}
+
+struct Neuron {
+    bias: f32,
+    weights: Vec<f32>,
+}
+
+pub struct LayerTopology {
+    pub input_neurons: usize,
+    pub output_neurons: usize,
+}
 
 impl Network {
-    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
-        todo!()
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+
+    pub fn new(layers: Vec<Layer>) -> Self {
+        Self { layers }
+    }
+}
+
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()
+    }
+
+    pub fn random(input_neurons: usize, output_neurons: usize) -> Self {
+        let mut rng = rand::thread_rng();
+
+        let bias = rng.gen_range(-1.0..=1.0);
+
+        let neurons = (0..output_neurons)
+            .map(|_| Neuron::random(input_neurons))
+            .collect();
+
+        Self { neurons }
+    }
+}
+
+impl Neuron {
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+
+        (self.bias + output).max(0.0)
     }
 }
